4ecc55f7a7494ce4ef593754dd57574b5cc2d7ee
zul-adn <zbojetx@gmail.com>
2021-10-07 16:33:20 +0700

    add row

--- src/App/Screens/Receptionist/Page2/index.js
+++ src/App/Screens/Receptionist/Page2/index.js
@@ -10,31 +10,38 @@ export default function Index() {
     return (
         <div>
             <Header />
-            <Card
-                title={receptionist_body.page2.basic}
-            >
-                <div className="body">
-                    <Upload
-                        label={form_label.logo}
-                    />
-                    <Input
-                        label={form_label.pin}
-                        placeholder=""
-                    />
+            <div className="page2-container">
+                <div className="section1">
+                    <Card
+                        title={receptionist_body.page2.basic}
+                    >
+                        <div className="body">
+                            <Upload
+                                label={form_label.logo}
+                            />
+                            <Input
+                                label={form_label.pin}
+                                placeholder=""
+                            />
+                        </div>
+                    </Card>
+
+                    <Card
+                        title={receptionist_body.page2.screen}
+                    >
+                        aaaa
+                    </Card>
+
+                    <Card
+                        title={receptionist_body.page2.staff}
+                    >
+                        aaaa
+                    </Card>
                 </div>
-            </Card>
-
-            <Card
-                title={receptionist_body.page2.screen}
-            >
-                aaaa
-            </Card>
-
-            <Card
-                title={receptionist_body.page2.staff}
-            >
-                aaaa
-            </Card>
+                <div className="section2">
+                aaa
+                </div>
+            </div>
         </div>
     )
 }
